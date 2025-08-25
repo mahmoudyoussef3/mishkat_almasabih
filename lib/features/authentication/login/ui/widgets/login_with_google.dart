bc589390cb4874f5d615a7a49b4ff07921af2375
mahmoudyoussef3 <hhfjfjfj35@gmail.com>
2025-08-25 16:45:40 +0300

    make edits in login with gogle

--- lib/features/authentication/login/ui/widgets/login_with_google.dart
+++ lib/features/authentication/login/ui/widgets/login_with_google.dart
@@ -1,4 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:mishkat_almasabih/core/networking/dio_factory.dart';
 import 'package:mishkat_almasabih/core/theming/colors.dart';
 
 class LoginWithGoogle extends StatelessWidget {
@@ -6,27 +9,64 @@ class LoginWithGoogle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Colors.white,
-      elevation: 5,
-      child: Padding(
-        padding: EdgeInsets.all(16),
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            //  border: Border.all(color: ColorsManager.primaryGreen),
-          ),
-          child: Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+    return InkWell(
+      onTap: () async {
+        
 
-              children: [
-                Text(
-                  'تسجيل الدخول عبر جوجل',
-                  style: TextStyle(color: ColorsManager.darkGray),
-                ),
-                Icon(Icons.apple, color: ColorsManager.darkGray),
-              ],
+final GoogleSignIn _googleSignIn = GoogleSignIn(
+  scopes: ['email', 'profile'],
+  clientId: "479373165372-kfm8ch3rt17kod1qk0uva7g9b7e39ue1.apps.googleusercontent.com",
+);
+
+  try {
+    final GoogleSignInAccount? account = await _googleSignIn.signIn();
+    if (account != null) {
+      final GoogleSignInAuthentication auth = await account.authentication;
+
+      // ده التوكين اللي تبعته للباك إند
+      final idToken = auth.idToken;
+final Dio dio = Dio(
+  BaseOptions(
+    baseUrl: "https://api.hadith-shareef.com/api/", // غير ده لعنوان السيرفر بتاعك
+    connectTimeout: const Duration(seconds: 10),
+    receiveTimeout: const Duration(seconds: 10),
+  ),
+);
+      // ابعته للـ backend API بتاعك
+          final response = await dio.post(
+      "/auth/google", // Endpoint عندك في السيرفر
+      data: {"id_token": idToken},
+    );
+
+      print("Backend response: ${response.data}");
+    }
+  } catch (error) {
+    print("Google Sign-In Error: $error");
+  }
+
+      },
+      child: Card(
+        color: Colors.white,
+        elevation: 5,
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              //  border: Border.all(color: ColorsManager.primaryGreen),
+            ),
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+      
+                children: [
+                  Text(
+                    'تسجيل الدخول عبر جوجل',
+                    style: TextStyle(color: ColorsManager.darkGray),
+                  ),
+                  Icon(Icons.apple, color: ColorsManager.darkGray),
+                ],
+              ),
             ),
           ),
         ),
